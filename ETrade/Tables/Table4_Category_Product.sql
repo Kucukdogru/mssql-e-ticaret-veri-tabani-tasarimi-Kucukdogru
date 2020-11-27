feb1befa79c1800973001e728de1dfc768ebfceb
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2020-11-27 15:54:17 +0300

    Update Table4_Category_Product.sql

--- ETrade/Tables/Table4_Category_Product.sql
+++ ETrade/Tables/Table4_Category_Product.sql
@@ -10,4 +10,4 @@ CategoryId int NOT NULL,
 Constraint pk_category_product PRIMARY KEY(CategoryProductId),
 Constraint fk_category_product_productId FOREIGN KEY(ProductId) REFERENCES Product(ProductId),
 Constraint fk_category_product_categoryId FOREIGN KEY(CategoryId) REFERENCES Category(CategoryId)
-);
+)
