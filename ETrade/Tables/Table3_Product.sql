e4d3fc4a0ec75d4bc19888fffe78477c9cd1ab58
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2020-11-30 22:33:31 +0300

    Update Table3_Product.sql
    ModelId column added.

--- ETrade/Tables/Table3_Product.sql
+++ ETrade/Tables/Table3_Product.sql
@@ -5,6 +5,7 @@ CREATE TABLE Product(
 ProductId int identity,
 ProductName nvarchar(200),
 BrandId int NOT NULL,
+ModelId int NOT NULL,
 ProductQuantity int,
 PurchasePrice decimal,
 SalePrice decimal,
@@ -14,5 +15,6 @@ UpdateDate date,
 
 
 Constraint pk_product_id PRIMARY KEY(ProductId),
-Constraint fk_product_brand_id FOREIGN KEY(BrandId) REFERENCES Brand(BrandId)
+Constraint fk_product_brand_id FOREIGN KEY(BrandId) REFERENCES Brand(BrandId),
+Constraint fk_product_model_id FOREIGN KEY(ModelId) REFERENCES Model(ModelId)
 )
